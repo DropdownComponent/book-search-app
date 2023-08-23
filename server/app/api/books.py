b6d98f2213ddfc4132a2f9cf8ea323d336e0edf3
myrat207 <myrat.mammedov207@gmail.com>
2023-08-23 18:37:19 -0500

    Reecent Search functionality has been added

--- server/app/api/books.py
+++ server/app/api/books.py
@@ -3,6 +3,10 @@ from app.services import google_books
 
 router = APIRouter()
 
+@router.get("/recent-searches/")
+def get_recent_searches():
+    return {"recent_searches": google_books.RECENT_SEARCHES}
+
 @router.get("/search/{query}")
 async def search_books(query: str):
     try:
--- server/app/services/google_books.py
+++ server/app/services/google_books.py
@@ -1,31 +1,16 @@
 import httpx
 
 BASE_URL = "https://www.googleapis.com/books/v1/volumes"
+RECENT_SEARCHES = []
+
+def add_to_recent_searches(query: str):
+    global RECENT_SEARCHES
+    RECENT_SEARCHES.append(query)
+    # Limit the size to the last 10 searches
+    RECENT_SEARCHES = RECENT_SEARCHES[-10:]
 
 async def search_google_books(query: str):
-    params = {"q": query}
-    async with httpx.AsyncClient() as client:
-        response = await client.get(BASE_URL, params=params)
-        data = response.json()
-
-        books = data.get("items", [])
-        processed_books = []
-        for book in books:
-            volume_info = book.get("volumeInfo", {})
-            title = volume_info.get("title", "N/A")
-            authors = volume_info.get("authors", ["Unknown"])
-            description = volume_info.get("description", "No description available.")
-            thumbnail = volume_info.get("imageLinks", {}).get("thumbnail", None)
-
-            processed_books.append({
-                "title": title,
-                "authors": authors,
-                "description": description,
-                "thumbnail": thumbnail
-            })
-
-        return processed_books
-async def search_google_books(query: str):
+    add_to_recent_searches(query)
     params = {"q": query}
     async with httpx.AsyncClient() as client:
         response = await client.get(BASE_URL, params=params)
