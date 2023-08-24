7581941a5da217af5f0faa654279c53b83d9f954
myrat207 <myrat.mammedov207@gmail.com>
2023-08-24 00:56:28 -0500

    suggested search results based on search history has been removed.

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,8 +3,6 @@ import { useEffect, useState, useRef, useCallback } from 'react';
 export default function Home() {
   const [query, setQuery] = useState("");
   const [books, setBooks] = useState([]);
-  const [recentSearches, setRecentSearches] = useState([]);
-  const [showSuggestions, setShowSuggestions] = useState(false);
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
   const [page, setPage] = useState(0);
@@ -22,21 +20,30 @@ export default function Home() {
   }, [loading, hasMore]);
 
   const handleSearch = () => {
+    if (!query.trim()) {
+        return;
+    }
     setBooks([]);
     setPage(0);
     setHasMore(true);
     searchBooks(0);
-    fetchRecentSearches();
-  };
+};
 
   const searchBooks = async (page = 0) => {
     setLoading(true);
     const response = await fetch(`http://127.0.0.1:8000/books/search/${query}?startIndex=${page * 10}`);
     const data = await response.json();
-    setBooks(prevBooks => [...prevBooks, ...data]);
+
+    if (Array.isArray(data)) {
+        setBooks(prevBooks => [...prevBooks, ...data]);
+        if (data.length < 10) setHasMore(false);
+    } else {
+        setHasMore(false);
+    }
+
     setLoading(false);
-    if (data.length < 10) setHasMore(false);
-  };
+};
+
 
   useEffect(() => {
     if (page > 0) {
@@ -44,40 +51,20 @@ export default function Home() {
     }
   }, [page]);
 
-  const fetchRecentSearches = async () => {
-    const response = await fetch('http://127.0.0.1:8000/books/recent-searches');
-    const data = await response.json();
-    setRecentSearches(data.recent_searches);
-    console.log("Fetched recent searches:", data.recent_searches);
-  };
-
   return (
     <div className="container mx-auto p-4">
-      <div className="relative">
       <input 
-    type="text" 
-    value={query}
-    onChange={(e) => setQuery(e.target.value)}
-    onKeyDown={(e) => {
-        if (e.key === 'Enter') {
-            handleSearch();
-        }
-    }}
-    onFocus={() => setShowSuggestions(true)}
-    onBlur={() => setTimeout(() => setShowSuggestions(false), 200)}
-    className="p-2 border rounded w-full"
-    placeholder="Search for books..."
-/>
-        {showSuggestions && (
-          <div className="border rounded mt-2 absolute left-0 bg-white z-10 w-full">
-            {recentSearches.map((search, index) => (
-              <div key={index} onClick={() => setQuery(search)} className="p-2 hover:bg-gray-200 cursor-pointer">
-                {search}
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+        type="text" 
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+                handleSearch();
+            }
+        }}
+        className="p-2 border rounded w-full"
+        placeholder="Search for books..."
+      />
       <button onClick={handleSearch} className="p-2 bg-blue-500 text-white ml-2 mt-2 rounded">Search</button>
 
       <div className="flex flex-wrap justify-center mt-4">
@@ -100,5 +87,5 @@ export default function Home() {
       </div>
       {loading && <div className="text-center mt-4 text-lg">Loading more books...</div>}
     </div>
-);
+  );
 }
